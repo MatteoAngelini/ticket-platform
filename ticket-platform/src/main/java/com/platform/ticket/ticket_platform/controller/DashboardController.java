63dbd3b08a0790b66e952f89ca229f4c2dbb2f60
topqualityconsulenza <marketing@consulenzatopquality.com>
2025-07-25 17:57:36 +0200

    creazione pagina idashboard admin

--- ticket-platform/src/main/java/com/platform/ticket/ticket_platform/controller/DashboardController.java
+++ ticket-platform/src/main/java/com/platform/ticket/ticket_platform/controller/DashboardController.java
@@ -1,12 +1,44 @@
 package com.platform.ticket.ticket_platform.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.platform.ticket.ticket_platform.model.Ticket;
+import com.platform.ticket.ticket_platform.repository.TicketRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
-@RequestMapping("/dashboard")
+@RequestMapping("/")
 public class DashboardController {
 
+    @Autowired
+    private TicketRepository ticketRepository;
+
+
+    @GetMapping
+    public String index(Model model, @RequestParam(value = "keyword", required = false) String keyword) {
+        
+        List<Ticket> tickets;
+
+        if (keyword != null && !keyword.isEmpty()) {
+            tickets = ticketRepository.findByTitleContainingIgnoreCase(keyword);
+        }else{
+            tickets = ticketRepository.findAll();
+        }
+
+        model.addAttribute("tickets", tickets);
+        model.addAttribute("keyword", keyword);
+
+        return "/dashboard/index";
+    }
     
     
 }
--- ticket-platform/src/main/java/com/platform/ticket/ticket_platform/repository/TicketRepository.java
+++ ticket-platform/src/main/java/com/platform/ticket/ticket_platform/repository/TicketRepository.java
@@ -1,9 +1,13 @@
 package com.platform.ticket.ticket_platform.repository;
 
+import java.util.List;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.platform.ticket.ticket_platform.model.Ticket;
 
 public interface TicketRepository extends JpaRepository<Ticket,Integer>{
     
+    List<Ticket> findByTitleContainingIgnoreCase(String keyword);
 }
